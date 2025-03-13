fe1f371c715178a34db38dd179ce5bb6e6e8faf8
desivar <your.email@example.com>
2025-03-12 21:38:28 -0600

    contacts

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -4,10 +4,10 @@ const objectId = require('mongodb').ObjectId;
  
 const getAll=async(req,res)=>{
  
-    const result = await mongodb.getDB().db().collection('users').find();
-    result.toArray().then((users)=>{
+    const result = await mongodb.getDB().db().collection('contacts').find();
+    result.toArray().then((contacts)=>{
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users);
+        res.status(200).json(contacs);
     });
 };
  
@@ -15,9 +15,9 @@ const getAll=async(req,res)=>{
 const getSingle=async(req,res)=>{
     const userId=new ObjectId(req.params.id);
     const result = await mongodb.getDB().db().collection('users').find({ _id: userId });
-    result.toArray().then((users)=>{
+    result.toArray().then((contacts)=>{
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users);
+        res.status(200).json(contacts);
     });
 };
     module.exports={
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,6 @@ router.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-router.use('/users', require('./users'));
+router.use('/contacts', require('./contacts'));
 
 module.exports = router;
