ad10abcc30c796aa8c7a70921775fa2dff9d9436
desivar <jillian_desire@yahoo.es>
2025-05-11 16:27:56 -0600

    add routes/index

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+
+
 const port =process.env.PORT || 5500;
 
 
+
+app.use('/', require('./routes'));
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
     }
