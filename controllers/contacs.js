b6865793420a8bd153dfb33fece6f28e0adc0e22
desivar <jillian_desire@yahoo.es>
2025-05-12 22:03:13 -0600

    fixed files

--- controllers/contacs.js
+++ controllers/contacs.js
@@ -8,5 +8,8 @@ const getAll = async (req, res) => {
 
 const getSingle = async (req, res) => {
 const result = await mongodb.getDatabase().db().collection('contacts').find();
-    
+result.toArray().then(contacts => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(contacts);
+});
 }
