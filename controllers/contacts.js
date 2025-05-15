647d30a2b70c3c964783d92f82a0a107434a7ac8
desivar <jillian_desire@yahoo.es>
2025-05-15 07:31:18 -0600

    updated contacts controller to handlevalidation

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,50 +1,44 @@
 const mongodb = require('../data/database');
-const { ObjectId } = require('mongodb');
+const Objectid=require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-  try {
-    const result = await mongodb.getDatabase().collection('contacts').find();
-    const contacts = await result.toArray();
+  const result=await mongodb.getDb().db().collection('contacts').find();
+  result.toArray().then((contacts) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(contacts);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+  });
 };
 
 const getSingle = async (req, res) => {
-  try {
-    if (!req.params.id || !ObjectId.isValid(req.params.id)) {
-      return res.status(400).json({ message: 'Invalid contact ID' });
-    }
-    
-    const contactId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().collection('contacts').find({ _id: contactId });
-    const contacts = await result.toArray();
-    
-    if (contacts.length === 0) {
-      return res.status(404).json({ message: 'Contact not found' });
-    }
-    
+  const contactId = new Objectid(req.params.id);
+  const result = await mongodb.getDb().db().collection('contacts').find({ _id: contactId });
+  result.toArray().then((contacts) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(contacts[0]);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+  });
 };
-
-
 const createContact = async (req, res) => {
-const { firstName, lastName, email, favoriteColor, birthday } = req.body;
-
-}
+  const userId= new Objectid(req.params.id);
+  const contact = {
+    firstNme: req.body.firstName,
+    lastNme: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  const response = await mongodb.getDb().db().collection('contacts').replaceOne(_id:contactId}, contact);
+  if (response.modifiedCount > 0) {
+    
+    res.status(204).send();
+  }
+  else{
+    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+  }
 
 module.exports = {
-
-
   getAll,
   getSingle,
   createContact,
   updateContact,
-  deleteContact
-};
+  this.deleteContact,
+} 
