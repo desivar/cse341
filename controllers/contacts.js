9feba7fb054ad3ef7015ee89ffed231f407f0acf
desivar <jillian_desire@yahoo.es>
2025-05-16 17:55:12 -0600

    updating

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,12 +2,15 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
+  console.log("Database connection:", mongodb.getDatabase()); // Debugging line
+  
   const result = await mongodb.getDatabase().collection('contacts').find();
   result.toArray().then((contacts) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(contacts);
   });
 };
+
 
 const getSingle = async (req, res) => {
   const contactId = new ObjectId(req.params.id);
