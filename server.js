865db74960f0baed2274d89a68f5ba59341d8232
desivar <jillian_desire@yahoo.es>
2025-05-16 07:57:36 -0600

    adding PORT 8080

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const { initDb } = require('./data/database'); // Correct import
 const app = express();
 
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
