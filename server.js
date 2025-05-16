893d2c305e479c41092b8b7458a3524b10c46723
desivar <jillian_desire@yahoo.es>
2025-05-15 23:41:36 -0600

    elimited redundance

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const contactsRoutes = require('./routes/contacts');
 
 app.use('/', routes);
 app.use('/contacts', contactsRoutes);
-app.use(bodyParser.json());
+
 
 console.log('All Environment Variables:', process.env); // <--- ADD THIS LINE HERE
 
