62807a07fce909336883ffd4209b9fc1a8c36db8
desivar <jillian_desire@yahoo.es>
2025-05-16 18:01:31 -0600

    routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 // Fix the path to the controller if needed
-const contactsController = require('../controllers/contacts');
+const contactsController = require('..contacts/controllers/contacts');
 
 router.get('/', contactsController.getAll);
 router.get('/:id', contactsController.getSingle);
