0e302cf9d503443ba03852300d9c4a97dad46cd7
desivar <jillian_desire@yahoo.es>
2025-05-21 16:51:00 -0600

    fix typos in routes/contacts.js

--- routes/contacts.js
+++ routes/contacts.js
@@ -8,9 +8,9 @@ router.get('/', contactsController.getAll);
 
 router.get('/:id', contactsController.getSingle);
 
-router.post('/', validation.saveContact, contactsController.createContact);
+router.post('/', contactsController.createContact);
 
-router.put('/:id', validation.saveContact, contactsController.updateContact);
+router.put('/:id', contactsController.updateContact);
 
 router.delete('/:id', contactsController.deleteContact);
 
