7d1a0f274409e2eaceb0676613009cd8328b80e5
desivar <jillian_desire@yahoo.es>
2025-05-21 16:33:27 -0600

    add swaggerr to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,13 @@
+const router = require('express').Router();
+router.use('/', require('./swagger')); // Swagger route
 
-
-const { Router } = require('express');
-const router = Router();
-
-// 1. Import your contacts router
-const contactsRoutes = require('./contacts'); // Make sure this path is correct relative to index.js
-
-// 2. Define a simple root route (optional, but good for testing your base URL)
 router.get('/', (req, res) => {
-    res.send('Welcome to the API! Your server is running.');
+  //#swagger.tags=[Hello World!]
+  res.send('Hello World!');
 });
+router.use('/contacts', require('./contacts')); // Contacts route
 
-// 3. Mount your contacts router under the '/contacts' path
-// This is the CRUCIAL line that connects your contacts API endpoints.
-router.use('/contacts', contactsRoutes);
+
 
 // Export the combined router
 module.exports = router;
