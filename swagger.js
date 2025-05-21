a427d0990c0e1ae4754a8b6b822cdcd151e126e5
desivar <jillian_desire@yahoo.es>
2025-05-20 18:55:15 -0600

    swagger.js updated

--- swagger.js
+++ swagger.js
@@ -1,21 +1,12 @@
 const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
-  info: {
-    title: 'My API',
-    description: 'Temple API'
-  },
-  host: 'cse341-instructor.herokuapp.com',
-  schemes: ['https']
+    info: {
+        title: 'Contacts API',
+        description: 'API for managing contacts'
+    },
+    host: 'localhost:5500',
+    schemes: ['http'],
 };
-
-const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index.js'];
-
-// generate swagger.json
-swaggerAutogen(outputFile, endpointsFiles, doc);
-
-// Run server after it gets generated
-// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
-//   await import('./index.js');
-// });
+const outputFile = './swagger_output.json';
+const endpointsFiles = ['./routes/index.js']; // Adjust the path to your routes file
